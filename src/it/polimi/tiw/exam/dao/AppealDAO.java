9337c1d0da20185b8a589668720816a23ee9d847
Federico <federico.gibellini@mail.polimi.it>
2021-04-17 11:03:49 +0200

    Minor fix in AppealDAO

--- src/it/polimi/tiw/exam/dao/AppealDAO.java
+++ src/it/polimi/tiw/exam/dao/AppealDAO.java
@@ -4,8 +4,6 @@ import java.sql.*;
 import java.util.*;
 import it.polimi.tiw.exam.objects.*;
 
-
-
 public class AppealDAO {
 	
 	private Connection connection;
@@ -16,7 +14,7 @@ public class AppealDAO {
 	}
 	
 	
-	public List<Appeal> getAppealsByCourse(Course course) throws SQLException {
+	public List<Appeal> getAppealsByCourse(Course course) throws SQLException { //needs Course Object
 		
 		String query = "SELECT * FROM Appeal WHERE id_course = ?";
 		PreparedStatement statement = null;
@@ -25,8 +23,8 @@ public class AppealDAO {
 		
 		try {
 			statement = connection.prepareStatement(query);
-			statement.setInt(1, course.getId()); 	//update this after seeing which getter for courseId
-												 	//is actually implemented in Course Object
+			statement.setInt(1, course.getCourseId()); 		//update this after seeing which getter for courseId
+												 			//is actually implemented in Course Object
 			rs = statement.executeQuery();
 			if(rs.next()==true) {
 				Appeal temp = new Appeal();		
