35804c2b03ad0b240f3a2605d6fe9470270a0f47
Federico <federico.gibellini@mail.polimi.it>
2021-06-03 12:20:25 +0200

    Introduce Appeal-section
    Introduced servlet to get appeals from the DB
    Updated HomeRIA.html and main.js to handle obtaining of appeals

--- WebContent/main.js
+++ WebContent/main.js
@@ -1,17 +1,15 @@
 (function() {
 
-  var courseList, pageOrchestrator;
+  var courseList, appealList, subscribers, pageOrchestrator;
 
   window.addEventListener("load", () => {
     pageOrchestrator = new PageOrchestrator();
     pageOrchestrator.start();
-    courseList = new CourseList();
-    courseList.clear();
-    courseList.start();
   });
 
 
   function CourseList(){
+    this.element = document.querySelector("div[class='courses']");
     this.message = document.querySelector("div[class='courses']>span");
     this.courses = document.querySelector("div[class='courses']>table>tbody");
     //introduce function to clear the table but the header row
@@ -20,6 +18,7 @@
         this.courses.removeChild(this.courses.children[0]);
       }
     }
+    this.clear();
     this.update = function update(req) {
       if (req.readyState === 4 && req.status === 200){
         let i = 0;
@@ -34,31 +33,123 @@
           let anchor = document.createElement("a");
           text = document.createTextNode("Show Appeals");
           anchor.appendChild(text);
-          anchor.setAttribute("href", "GetAppealsRIA"); //certainly needs updating
+          anchor.setAttribute("href", "#");
+          anchor.setAttribute("courseId", array[i].courseId);
+          appealLinkCell.appendChild(anchor);
+          newRow.appendChild(appealLinkCell);
+          //may want to check why in this exact moment this.courses is undefined
+          //this.courses = document.querySelector("div[class='courses']>table>tbody");
+          courseList.courses.appendChild(newRow);
+          anchor.addEventListener("click", (e) => {
+            e.preventDefault();
+            appealList.hide();
+            appealList.clear();
+            subscribers.hide();
+            subscribers.clear();
+            appealList.show(e.target.getAttribute('courseId'));
+          });
+        }
+      }
+    }
+    makeCall("GET", "GetCoursesRIA", this.update);
+  }
+
+  function AppealList(){
+    this.element = document.querySelector("div[class='appeals']");
+    this.message = document.querySelector("div[class='appeals']>span");
+    this.appeals = document.querySelector("div[class='appeals']>table>tbody");
+    //this.message = "Here are the appeals for the course \"" + title + "\":";
+    //this.appeals = appeals;
+    this.hide = function hide(){
+      this.element.style.display = "none";
+    }
+    this.show = function show(courseId){
+      makeCall("GET", "GetAppealsRIA?courseId=" + courseId, this.update);
+      this.element.removeAttribute("style");
+    }
+    this.clear = function clear() {
+      while(this.appeals.children.length>0){
+        this.appeals.removeChild(this.appeals.children[0]);
+      }
+    }
+    this.hide();
+    this.clear();
+    this.update = function update(req) {
+      if (req.readyState === 4 && req.status === 200){
+        let i = 0;
+        let array = JSON.parse(req.responseText);
+        for (i = 0; i<array.length; i++){
+          let newRow = document.createElement("tr");
+          let dateCell = document.createElement("td");
+          let anchor = document.createElement("a");
+          let text = document.createTextNode(array[i].date);
+          anchor.appendChild(text);
+          anchor.setAttribute("href", "#");
+          anchor.setAttribute("appealId", array[i].appealId);
+          dateCell.appendChild(anchor);
+          newRow.appendChild(dateCell);
+          appealList.appeals.appendChild(newRow);
+          //anchor.addEventListener();
+        }
+      }
+    }
+  }
+
+  function Subscribers() {
+    this.element = document.querySelector("div[class='subscribers']");
+    this.message = document.querySelector("div[class='subscribers']>span");
+    this.subs = document.querySelector("div[class='subscribers']>table>tbody");
+    this.hide = function hide(){
+      this.element.style.display = "none";
+    }
+    this.show = function show(){
+      this.element.removeAttribute("style");
+    }
+    this.clear = function clear() {
+      while(this.subs.children.length>0){
+        this.subs.removeChild(this.subs.children[0]);
+      }
+    }
+    this.hide();
+    this.clear();
+    this.update = function update(req) {
+      /*
+      if (req.readyState === 4 && req.status === 200){
+        let i = 0;
+        let array = JSON.parse(req.responseText);
+        for (i = 0; i<array.length; i++){
+          let newRow = document.createElement("tr");
+          let titleCell = document.createElement("td");
+          let text = document.createTextNode(array[i].title);
+          titleCell.appendChild(text);
+          newRow.appendChild(titleCell);
+          let appealLinkCell = document.createElement("td");
+          let anchor = document.createElement("a");
+          text = document.createTextNode("Show Appeals");
+          anchor.appendChild(text);
+          anchor.setAttribute("href", "GetAppealsRIA?courseId=" + array[i].courseId); //certainly needs updating
           appealLinkCell.appendChild(anchor);
           newRow.appendChild(appealLinkCell);
           //may want to check why in this exact moment this.courses is undefined
           this.courses = document.querySelector("div[class='courses']>table>tbody");
           this.courses.appendChild(newRow);
+          anchor.addEventListener();//implement*/
         }
-      }
-    }
-    this.start = function start(){
-      makeCall("GET", "GetCoursesRIA", this.update);
-    }
-
   }
-
-
 
   function PageOrchestrator(){
     this.start = function (){
+      courseList = new CourseList();
+      appealList = new AppealList();
+      subscribers = new Subscribers();
+      //this.clearAll();
+    }
+    this.clearAll = function clearAll() {
       let temp = document.querySelector("div[class='appeals']");
       temp.remove();
       temp = document.querySelector("div[class='subscribers']");
       temp.remove();
     }
-
   }
 
   function makeCall(method, url, callback) {
---
+++ src/it/polimi/tiw/exam/riaControllers/GetAppealsRIA.java
@@ -0,0 +1,96 @@
+package it.polimi.tiw.exam.riaControllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import com.google.gson.Gson;
+
+import it.polimi.tiw.exam.dao.AppealDAO;
+import it.polimi.tiw.exam.dao.CourseDAO;
+import it.polimi.tiw.exam.objects.Course;
+import it.polimi.tiw.exam.objects.User;
+import it.polimi.tiw.exam.objects.Appeal;
+import it.polimi.tiw.exam.utils.ConnectionHandler;
+import it.polimi.tiw.exam.utils.TemplateEngineHandler;
+
+
+@WebServlet("/GetAppealsRIA")
+public class GetAppealsRIA extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection = null;
+
+	public void init() throws ServletException {
+		connection = ConnectionHandler.getConnection(getServletContext());
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Integer cId = null;
+		try {
+			cId = Integer.parseInt(request.getParameter("courseId"));
+		} catch (NumberFormatException| NullPointerException e) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Incorrect param values");
+			return;
+		}
+		HttpSession session=request.getSession();
+		
+		CourseDAO coursesDAO = new CourseDAO(connection);
+		User user=null;
+		try {
+			//user=(User)session.getAttribute("user");
+			user = new User (3, "Professor");
+			if(coursesDAO.hasCourse(cId, user.getPersonId(), user.getAccessRights())==false) {
+				response.sendRedirect(getServletContext().getContextPath()+"/GetCourses");
+				return;
+			}
+		} catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Illegal course request");
+			return;
+		}
+		
+		Course course = null;
+		List<Appeal> appeals=new LinkedList<>();
+		try {
+			course = coursesDAO.getCourseById(cId);
+			if (course == null) {
+				response.sendError(HttpServletResponse.SC_NOT_FOUND, "Course not found");
+				return;
+			}
+			
+			AppealDAO appealDAO = new AppealDAO(connection);
+			appeals = appealDAO.getAppealsByCourse(course.getCourseId(),user.getPersonId(),user.getAccessRights());
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		String json = new Gson().toJson(appeals);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(json);
+	}
+
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
