38236e11245da69b26f3f03585f2a1c099c1a66b
Federico <federico.gibellini@mail.polimi.it>
2021-06-23 14:19:27 +0200

    Update Edit
    Introduced control on entered grade

--- src/it/polimi/tiw/exam/controllers/Edit.java
+++ src/it/polimi/tiw/exam/controllers/Edit.java
@@ -26,12 +26,13 @@ import it.polimi.tiw.exam.utils.ConnectionHandler;
 public class Edit extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection connection = null;
-	
+
 	public void init() throws ServletException {
 		connection = ConnectionHandler.getConnection(getServletContext());
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		User user = (User) session.getAttribute("user");
 		int appId;
@@ -40,57 +41,72 @@ public class Edit extends HttpServlet {
 		ErrorMsg error = (ErrorMsg) request.getAttribute("error");
 		// forward to GetSubscribers if an error has already occurred
 		RequestDispatcher rd = request.getRequestDispatcher("GetSubscribers");
-		
-		//controls on request parameters
+
+		// controls on request parameters
 		try {
-			appId=Integer.parseInt(request.getParameter("appeal"));
-			studentId=Integer.parseInt(request.getParameter("studentId"));
-			gradeValue=request.getParameter("gradeValue");
-		}catch(NumberFormatException e) {
-			error = new ErrorMsg(HttpServletResponse.SC_BAD_REQUEST, "Incorrect param values");
+			appId = Integer.parseInt(request.getParameter("appeal"));
+			studentId = Integer.parseInt(request.getParameter("studentId"));
+			gradeValue = request.getParameter("gradeValue");
+			// control on gradeValue validity
+			if (!gradeValue.equalsIgnoreCase("absent") && !gradeValue.equalsIgnoreCase("failed")
+					&& !gradeValue.equalsIgnoreCase("recalled") && !gradeValue.equalsIgnoreCase("18")
+					&& !gradeValue.equalsIgnoreCase("19") && !gradeValue.equalsIgnoreCase("20")
+					&& !gradeValue.equalsIgnoreCase("21") && !gradeValue.equalsIgnoreCase("22")
+					&& !gradeValue.equalsIgnoreCase("23") && !gradeValue.equalsIgnoreCase("24")
+					&& !gradeValue.equalsIgnoreCase("25") && !gradeValue.equalsIgnoreCase("26")
+					&& !gradeValue.equalsIgnoreCase("27") && !gradeValue.equalsIgnoreCase("28")
+					&& !gradeValue.equalsIgnoreCase("29") && !gradeValue.equalsIgnoreCase("30")
+					&& !gradeValue.equalsIgnoreCase("30 with merit"))
+				throw new Exception();
+		} catch (Exception e) {
+			error = new ErrorMsg(HttpServletResponse.SC_BAD_REQUEST, "Illegal request");
 			request.setAttribute("error", error);
 			rd.forward(request, response);
 			return;
 		}
-		
-		GradeDAO gradeDAO= new GradeDAO(connection);
+
+		GradeDAO gradeDAO = new GradeDAO(connection);
 		Grade grade;
-		
-		//retrieve a grade
+
+		// retrieve a grade
 		try {
-			grade=gradeDAO.getResultByAppealAndStudent(appId,studentId);
-			if(grade==null) throw new Exception();
-		}catch(Exception e) {
-			error = new ErrorMsg(HttpServletResponse.SC_BAD_REQUEST,"Nonexistent student");
+			grade = gradeDAO.getResultByAppealAndStudent(appId, studentId);
+			if (grade == null)
+				throw new Exception();
+		} catch (Exception e) {
+			error = new ErrorMsg(HttpServletResponse.SC_BAD_REQUEST, "Nonexistent student");
 			request.setAttribute("error", error);
 			rd.forward(request, response);
 			return;
 		}
-		
-		//control on state: if it's neither 'not entered' nor 'entered', won't edit grade
+
+		// control on state: if it's neither 'not entered' nor 'entered', won't edit
+		// grade
 		try {
-			if(!grade.getState().equalsIgnoreCase("entered")&&!grade.getState().equalsIgnoreCase("not entered")) throw new Exception();
-		}catch(Exception e) {
-			error = new ErrorMsg(HttpServletResponse.SC_BAD_REQUEST,"Uneditable grade");
+			if (!grade.getState().equalsIgnoreCase("entered") && !grade.getState().equalsIgnoreCase("not entered"))
+				throw new Exception();
+		} catch (Exception e) {
+			error = new ErrorMsg(HttpServletResponse.SC_BAD_REQUEST, "Uneditable grade");
 			request.setAttribute("error", error);
 			rd.forward(request, response);
 			return;
 		}
-		
-		//security: get last-visited student
-		SecurityDAO secDAO=new SecurityDAO(connection);
+
+		// security: get last-visited student
+		SecurityDAO secDAO = new SecurityDAO(connection);
 		try {
-			if(secDAO.getLastStudent(user.getPersonId())!=studentId) throw new Exception();
-		}catch(Exception e) {
+			if (secDAO.getLastStudent(user.getPersonId()) != studentId)
+				throw new Exception();
+		} catch (Exception e) {
 			error = new ErrorMsg(HttpServletResponse.SC_BAD_REQUEST, "Access denied for security reasons");
 			request.setAttribute("error", error);
 			rd.forward(request, response);
 			return;
 		}
-		
-		//enter grade
+
+		// enter grade
 		try {
-			gradeDAO.enterGrade(appId,studentId,gradeValue);
+			gradeDAO.enterGrade(appId, studentId, gradeValue);
 		} catch (SQLException e) {
 			error = new ErrorMsg(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
 					"An accidental error occurred while entering grades");
@@ -100,6 +116,7 @@ public class Edit extends HttpServlet {
 		}
 		response.sendRedirect("GetSubscribers?appeal=" + appId);
 	}
+
 	public void destroy() {
 		try {
 			ConnectionHandler.closeConnection(connection);
